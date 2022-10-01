99fb0ef8e65899181e4bbfb11514a13b79396b64
Vishal Sharma <48996501+Str4nge@users.noreply.github.com>
2022-10-01 09:45:55 +0530

    Create Transpose.cpp

---
+++ C_Cpp/Transpose.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Define REP () here to transpose the matrix and display the transpose.
+
+int main ()
+{
+   int r,c,i,j,a=0,b=0,arr[10][10];
+   cout<<"Enter the number of rows and coloumns :";
+   cin>>r>>c;
+   cout<<"Enter the array elements :";
+  
+   cout<<"The transform of the matrix is: "<<endl;
+  
+   REP(r,c,arr);
+  
+}
