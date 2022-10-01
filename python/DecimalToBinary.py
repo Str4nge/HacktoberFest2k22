18bb678a8aaf25c50520f97582fdd2abf2241511
Vishal Sharma <48996501+Str4nge@users.noreply.github.com>
2022-10-01 09:29:38 +0530

    Create DecimalToBinary.py

---
+++ python/DecimalToBinary.py
@@ -0,0 +1,7 @@
+#Complete the DecimalToBinary() here.
+
+def main():
+    x = int(input("Enter Decimal Number: "))
+    BinaryNumb = DecimalToBinary(x)
+    print("Binary Number: ",BinaryNumb)
+main()
